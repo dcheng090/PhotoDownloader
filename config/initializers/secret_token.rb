b3fc84a3ad0bb4e9eb21cb0dab1f74b256d69f34
Derrick Cheng <dcheng090@gmail.com>
2012-05-15 22:06:36 -0700

    initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoDownloader::Application.config.secret_token = 'd884bb9aa09b2bbf6207d383ebbdfd047bb35f57a312204935c786da0e20ccb5a4da614ffd0c1c90b2bdd502e9bb375b2294bfb82508f47f9a21bf3f2aad1b60'
